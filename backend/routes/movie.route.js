22bda0ad0eed388cacd375f5860acd64d7279191
FanchonSora <khanhngan1491@gmail.com>
2025-02-18 01:11:28 +0700

    fetch trending movie and movie api

---
+++ backend/routes/movie.route.js
@@ -0,0 +1,9 @@
+import express from 'express';
+import { getTrendingMovies } from '../controllers/movie.controller.js';
+
+const router = express.Router();
+
+router.get('/trending', getTrendingMovies);
+
+
+export default router;
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 import express from 'express';
 
 import authRoutes from './routes/auth.route.js';
+import movieRoutes from './routes/movie.route.js';
+
 import { ENV_VARS } from './config/envVars.js';
 import { connectDB } from './config/db.js';
 
@@ -9,8 +11,10 @@ const PORT = ENV_VARS.PORT;
 
 app.use(express.json()); //allow to parse req body
 app.use("/api/version1/auth", authRoutes);
+app.use("/api/version1/movie", movieRoutes);
 
 app.listen(PORT, () => {
     console.log('Server is running on http://localhost:' + PORT);
     connectDB();
 });
+
