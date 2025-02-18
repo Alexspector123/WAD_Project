332cb1adc2fd5fdbc340ed20b757a0196f6f44db
FanchonSora <khanhngan1491@gmail.com>
2025-02-18 16:57:21 +0700

    2/44/39

---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,10 @@
+// src/pages/HomePage.jsx
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div className="hero-bg h-screen"></div>
+  );
+};
+
+export default HomePage;
---
+++ frontend/src/pages/LoginPage.jsx
@@ -0,0 +1,12 @@
+// src/pages/LoginPage.jsx
+import React from 'react';
+
+const LoginPage = () => {
+  return (
+    <div className="text-center mt-10">
+      <h1 className="text-4xl font-bold">Login Page</h1>
+    </div>
+  );
+};
+
+export default LoginPage;
